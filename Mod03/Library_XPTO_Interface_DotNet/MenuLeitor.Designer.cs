5371249763ceb4c212f1ebade614923a02cadd93
JaqueProenca <99728859+JaquelineLucio@users.noreply.github.com>
2022-04-02 14:02:24 +0100

    Create MenuLeitor.Designer.cs

---
+++ Mod03/Library_XPTO_Interface_DotNet/MenuLeitor.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace XPTO_Interface
+{
+    partial class MenuLeitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVerificarSituação = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbNucleo = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbAssunto = new System.Windows.Forms.ComboBox();
+            this.btnHistorico = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(264, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seja Bem-Vindo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(436, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ".";
+            // 
+            // btnVerificarSituação
+            // 
+            this.btnVerificarSituação.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificarSituação.Location = new System.Drawing.Point(99, 204);
+            this.btnVerificarSituação.Name = "btnVerificarSituação";
+            this.btnVerificarSituação.Size = new System.Drawing.Size(127, 85);
+            this.btnVerificarSituação.TabIndex = 6;
+            this.btnVerificarSituação.Text = "Verificar situação";
+            this.btnVerificarSituação.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(288, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Pesquisar obras disponiveis:";
+            // 
+            // cbNucleo
+            // 
+            this.cbNucleo.FormattingEnabled = true;
+            this.cbNucleo.Items.AddRange(new object[] {
+            "Hora da Leitura - Faro",
+            "Ler para crer - Lisboa",
+            "Historia para boi dormir - Porto"});
+            this.cbNucleo.Location = new System.Drawing.Point(105, 143);
+            this.cbNucleo.Name = "cbNucleo";
+            this.cbNucleo.Size = new System.Drawing.Size(121, 21);
+            this.cbNucleo.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(101, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nucleo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(277, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Assunto:";
+            // 
+            // cbAssunto
+            // 
+            this.cbAssunto.FormattingEnabled = true;
+            this.cbAssunto.Items.AddRange(new object[] {
+            "Autoajuda",
+            "Distopia",
+            "Romance"});
+            this.cbAssunto.Location = new System.Drawing.Point(281, 143);
+            this.cbAssunto.Name = "cbAssunto";
+            this.cbAssunto.Size = new System.Drawing.Size(121, 21);
+            this.cbAssunto.TabIndex = 11;
+            // 
+            // btnHistorico
+            // 
+            this.btnHistorico.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHistorico.Location = new System.Drawing.Point(275, 204);
+            this.btnHistorico.Name = "btnHistorico";
+            this.btnHistorico.Size = new System.Drawing.Size(127, 85);
+            this.btnHistorico.TabIndex = 13;
+            this.btnHistorico.Text = "Historico";
+            this.btnHistorico.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(99, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 85);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Cancelar inscrição";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(275, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 85);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(470, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(303, 330);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // MenuLeitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnHistorico);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbAssunto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbNucleo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnVerificarSituação);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MenuLeitor";
+            this.Text = "MenuLeitor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnVerificarSituação;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbNucleo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbAssunto;
+        private System.Windows.Forms.Button btnHistorico;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
