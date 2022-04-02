681f193a8b8a90b545ee225c38c09b8fe84095ea
JaqueProenca <99728859+JaquelineLucio@users.noreply.github.com>
2022-04-02 14:02:55 +0100

    Create cadastro.Designer.cs

---
+++ Mod03/Library_XPTO_Interface_DotNet/cadastro.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace XPTO_Interface
+{
+    partial class Cadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtsenhaConf = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNIF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btCadastrarLeitor = new System.Windows.Forms.Button();
+            this.btnLimparFormCad = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(468, 344);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 20);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Confirme sua senha:";
+            // 
+            // txtsenhaConf
+            // 
+            this.txtsenhaConf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtsenhaConf.Location = new System.Drawing.Point(473, 375);
+            this.txtsenhaConf.Margin = new System.Windows.Forms.Padding(6);
+            this.txtsenhaConf.Name = "txtsenhaConf";
+            this.txtsenhaConf.Size = new System.Drawing.Size(212, 26);
+            this.txtsenhaConf.TabIndex = 24;
+            this.txtsenhaConf.UseSystemPasswordChar = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(468, 232);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Senha:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtSenha.Location = new System.Drawing.Point(473, 263);
+            this.txtSenha.Margin = new System.Windows.Forms.Padding(6);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(212, 26);
+            this.txtSenha.TabIndex = 22;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(468, 130);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtLogin.Location = new System.Drawing.Point(473, 161);
+            this.txtLogin.Margin = new System.Windows.Forms.Padding(6);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(212, 26);
+            this.txtLogin.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(116, 232);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "NIF:";
+            // 
+            // txtDn
+            // 
+            this.txtDn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtDn.Location = new System.Drawing.Point(120, 375);
+            this.txtDn.Margin = new System.Windows.Forms.Padding(6);
+            this.txtDn.Name = "txtDn";
+            this.txtDn.Size = new System.Drawing.Size(212, 26);
+            this.txtDn.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(116, 344);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Data de nascimento:";
+            // 
+            // txtNIF
+            // 
+            this.txtNIF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNIF.Location = new System.Drawing.Point(120, 263);
+            this.txtNIF.Margin = new System.Windows.Forms.Padding(6);
+            this.txtNIF.Name = "txtNIF";
+            this.txtNIF.Size = new System.Drawing.Size(212, 26);
+            this.txtNIF.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(296, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 29);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Seja Bem-Vindo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 130);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(121, 161);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(6);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(212, 26);
+            this.txtNome.TabIndex = 13;
+            // 
+            // btCadastrarLeitor
+            // 
+            this.btCadastrarLeitor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCadastrarLeitor.Location = new System.Drawing.Point(327, 422);
+            this.btCadastrarLeitor.Name = "btCadastrarLeitor";
+            this.btCadastrarLeitor.Size = new System.Drawing.Size(144, 90);
+            this.btCadastrarLeitor.TabIndex = 26;
+            this.btCadastrarLeitor.Text = "Cadastrar";
+            this.btCadastrarLeitor.UseVisualStyleBackColor = true;
+            this.btCadastrarLeitor.Click += new System.EventHandler(this.btCadastrarLeitor_Click);
+            // 
+            // btnLimparFormCad
+            // 
+            this.btnLimparFormCad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimparFormCad.Location = new System.Drawing.Point(155, 434);
+            this.btnLimparFormCad.Name = "btnLimparFormCad";
+            this.btnLimparFormCad.Size = new System.Drawing.Size(128, 66);
+            this.btnLimparFormCad.TabIndex = 27;
+            this.btnLimparFormCad.Text = "Limpar";
+            this.btnLimparFormCad.UseVisualStyleBackColor = true;
+            this.btnLimparFormCad.Click += new System.EventHandler(this.btnLimparFormCad_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(515, 434);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(128, 66);
+            this.btnVoltar.TabIndex = 28;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 556);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnLimparFormCad);
+            this.Controls.Add(this.btCadastrarLeitor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtsenhaConf);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNIF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Cadastro";
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Cadastro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtsenhaConf;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNIF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btCadastrarLeitor;
+        private System.Windows.Forms.Button btnLimparFormCad;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
