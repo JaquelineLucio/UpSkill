faba316e19d905d2a16d77d3067961ad5c4ece3b
Jaqueline Lucio <99728859+JaquelineLucio@users.noreply.github.com>
2022-04-12 12:20:05 +0100

    Create Person.cs

---
+++ Mod03/Classes_Inheritance/Parte4_Diagramas/Diagramas/Diagramas/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagramas
+{
+
+
+
+    abstract class Person
+    {
+    }
+    internal class Course
+    {
+    }
+    internal class Classroom
+    {
+    }
+    /*----------------------------------------*/
+
+    internal class Object : Classroom
+    {
+    }
+
+    /*--------------------------------------*/
+    internal class Student : Person
+    {
+    }  
+    internal class Employee : Person
+    {
+    }
+    /*---------------------------------------*/
+    internal class GraduateStudent : Student
+    {
+    }
+    /*---------------------------------------*/
+    internal class Manager : Employee
+    {
+    }
+    internal class Instructor : Employee
+    {
+    }
+    internal class Secretary : Employee
+    {
+    }
+    internal class TeachingAssistant : Employee
+    {
+    }
+    internal class Janitor : Employee
+    {
+    }
+    /*--------------------------------------*/
+    internal class DepartmentChair : Course
+    {
+    }
+    internal class Lecture : DepartmentChair
+    {
+    }
+    internal class Seminar : DepartmentChair
+    {
+    }
+    internal class SeminarSpeaker : Seminar
+    {
+    }
+    internal class ComputerLab : Classroom
+    {
+    }
+}
