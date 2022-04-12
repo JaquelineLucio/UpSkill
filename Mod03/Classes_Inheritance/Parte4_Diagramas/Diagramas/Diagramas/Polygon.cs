4aa245ff4f955f779f4c6a3caf5a29b36d44d04a
Jaqueline Lucio <99728859+JaquelineLucio@users.noreply.github.com>
2022-04-12 12:20:15 +0100

    Create Polygon.cs

---
+++ Mod03/Classes_Inheritance/Parte4_Diagramas/Diagramas/Diagramas/Polygon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagramas
+{
+    abstract class Polygon
+    {
+    }
+    internal class Triangle : Polygon
+    {
+    }
+    internal class Hexágono : Polygon
+    {
+    }
+}
