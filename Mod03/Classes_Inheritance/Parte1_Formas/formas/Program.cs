c13498f501aed308e32676c574e94c4bc73652d2
Jaqueline Lucio <99728859+JaquelineLucio@users.noreply.github.com>
2022-04-12 12:19:47 +0100

    Create Program.cs

---
+++ Mod03/Classes_Inheritance/Parte1_Formas/formas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formas
+{
+    internal class Program
+    {
+        static void testeFiguras()
+        {
+           // Circle c = new Circle(p,5);
+            Triangle t = new Triangle(new Point(3, 4), new Point(5, 7), new Point(0, 14));
+            Rectangle r = new Rectangle(new Point(3, 4), new Point(5, 7), new Point(0, 14), new Point (5,4)); ;
+        }
+        static void Main(string[] args)
+        {
+            testeFiguras();
+        }
+    }
+}
