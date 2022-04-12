b610bae2bbb22082b3030775cb1e779f699b8a46
Jaqueline Lucio <99728859+JaquelineLucio@users.noreply.github.com>
2022-04-12 12:20:20 +0100

    Create Vehicle.cs

---
+++ Mod03/Classes_Inheritance/Parte4_Diagramas/Diagramas/Diagramas/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagramas
+{
+
+/** 
+* PickupTruck
+* */
+    abstract class Vehicle
+    {
+    }
+    internal class car : Vehicle
+    {
+    }
+    /*--------------------------------*/
+    internal class Sedan : car
+    {
+    }
+    internal class Coupe : car
+    {
+    }
+    internal class SportUtilityVehicle : car
+    {
+    }
+    /*----------------------------------*/
+    internal class Bicycle : Vehicle
+    {
+    }
+    internal class Motorcycle : Vehicle
+    {
+    }
+    internal class Truck : Vehicle
+    {
+    }
+    internal class Minivan : Vehicle
+    {
+    }
+}
